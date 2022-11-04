8decabc7c6c6921e18014e5e068aa349c9a7eb6f
RimuruDev <Rimuru.Dev@gmail.com>
2022-11-04 17:33:56 +0600

    Shortening code with a lambda expression [Utils.cs]

--- Assets/SCP/Scripts/Utility/Utils.cs
+++ Assets/SCP/Scripts/Utility/Utils.cs
@@ -3,8 +3,5 @@ using UnityEngine;
 
 public static class Utils
 {
-    public static T GetRandomElement<T>( T[] array )
-    {
-        return array[Random.Range( 0, array.Length )];
-    }
+    public static T GetRandomElement<T>(T[] array) => array[Random.Range(0, array.Length)];
 }
