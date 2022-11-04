46034b6982a12b2e13e90f37b6ee5e944bd37155
Guthen <114919245+arkaht@users.noreply.github.com>
2022-11-04 09:53:08 +0100

    [added] door open & close sounds

--- Assets/SCP/Scripts/Door.cs
+++ Assets/SCP/Scripts/Door.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+	public AudioClip[] OpenSound, CloseSound;
+
 	bool isOpen = false;
 
+	new AudioSource audio;
 	Animator animator;
 
 	void Awake()
 	{
+		audio = GetComponent<AudioSource>();
 		animator = GetComponent<Animator>(); 
 	}
 
 	public void SetToggle( bool is_open )
 	{
 		isOpen = is_open;
+		
 		animator.SetTrigger( is_open ? "Open" : "Close" );
+
+		audio.PlayOneShot( isOpen ? OpenSound[Random.Range( 0, OpenSound.Length )] : CloseSound[Random.Range( 0, CloseSound.Length )] );
 	}
 
 	public void Toggle()
