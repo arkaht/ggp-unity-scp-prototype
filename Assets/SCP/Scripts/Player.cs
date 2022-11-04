30983f2a258281d8e482ce65596094951e3817a1
RimuruDev <Rimuru.Dev@gmail.com>
2022-11-04 17:01:12 +0600

    Aligned the code

--- Assets/SCP/Scripts/Player.cs
+++ Assets/SCP/Scripts/Player.cs
@@ -8,185 +8,185 @@ using Cinemachine;
 
 public class Player : MonoBehaviour
 {
-	public static Player Instance { get; private set; }
-	
-	public Vector3 ViewPos => Camera.main.transform.position;
-	public Vector3 ViewDir => Camera.main.transform.forward;
+    public static Player Instance { get; private set; }
 
-	public UseableEntity UseEntity { get; set; }
+    public Vector3 ViewPos => Camera.main.transform.position;
+    public Vector3 ViewDir => Camera.main.transform.forward;
 
-	public readonly Dictionary<int, Item> Inventory = new();
-	public int MaxInventorySlots = 8;
-	public bool IsInventoryFull => Inventory.Count > MaxInventorySlots;
+    public UseableEntity UseEntity { get; set; }
 
-	[Header( "Camera Noise" )]
-	public CinemachineVirtualCamera CinemachineVC;
-	public float DefaultCameraNoiseFrequency = .3f;
-	public float WalkCameraNoiseFrequency = 1.0f;
-	public float RunCameraNoiseFrequency = 3.0f;
-	public float SmoothNoiseFrequencySpeed = 3.0f;
+    public readonly Dictionary<int, Item> Inventory = new();
+    public int MaxInventorySlots = 8;
+    public bool IsInventoryFull => Inventory.Count > MaxInventorySlots;
 
-	float noiseFrequency = 0.0f;
+    [Header("Camera Noise")]
+    public CinemachineVirtualCamera CinemachineVC;
+    public float DefaultCameraNoiseFrequency = .3f;
+    public float WalkCameraNoiseFrequency = 1.0f;
+    public float RunCameraNoiseFrequency = 3.0f;
+    public float SmoothNoiseFrequencySpeed = 3.0f;
 
-	[Header( "Sounds" )]
-	public FootstepPlayer WalkFoostepPlayer;
-	public FootstepPlayer RunFoostepPlayer;
+    float noiseFrequency = 0.0f;
 
-	[Header( "Misc" )]
-	public float DropItemDistance = 2.0f;
+    [Header("Sounds")]
+    public FootstepPlayer WalkFoostepPlayer;
+    public FootstepPlayer RunFoostepPlayer;
 
-	StarterAssets.StarterAssetsInputs inputs;
-	CinemachineBasicMultiChannelPerlin cinemachineNoise;
+    [Header("Misc")]
+    public float DropItemDistance = 2.0f;
 
-	void Awake()
-	{
-		Instance = this;
+    StarterAssets.StarterAssetsInputs inputs;
+    CinemachineBasicMultiChannelPerlin cinemachineNoise;
 
-		inputs = GetComponent<StarterAssets.StarterAssetsInputs>();
-		cinemachineNoise = CinemachineVC.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-	}
+    void Awake()
+    {
+        Instance = this;
 
-	void OnWalkUpdate()
-	{
+        inputs = GetComponent<StarterAssets.StarterAssetsInputs>();
+        cinemachineNoise = CinemachineVC.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    void OnWalkUpdate()
+    {
         //  footsteps players
         WalkFoostepPlayer.enabled = true;
         RunFoostepPlayer.enabled = false;
 
-		//  get appropriate frequency
-		noiseFrequency = WalkCameraNoiseFrequency;
+        //  get appropriate frequency
+        noiseFrequency = WalkCameraNoiseFrequency;
     }
 
     void OnRunUpdate()
-	{
+    {
         //  footsteps players
         WalkFoostepPlayer.enabled = false;
         RunFoostepPlayer.enabled = true;
 
-		//  get appropriate frequency
-		noiseFrequency = RunCameraNoiseFrequency;
+        //  get appropriate frequency
+        noiseFrequency = RunCameraNoiseFrequency;
     }
 
-	void OnIdleUpdate()
-	{
+    void OnIdleUpdate()
+    {
         //  footsteps players
         WalkFoostepPlayer.enabled = false;
         RunFoostepPlayer.enabled = false;
 
-		//  get appropriate frequency
+        //  get appropriate frequency
         noiseFrequency = DefaultCameraNoiseFrequency;
     }
 
     void Update()
-	{
-		//  movement updates
-		if ( inputs.move != Vector2.zero )
-		{
-			if ( inputs.sprint )
-			{
-				OnRunUpdate();
-			}
-			else
-			{
-				OnWalkUpdate();
-			}
-		}
-		else
-		{
-			OnIdleUpdate();
-		}
+    {
+        //  movement updates
+        if (inputs.move != Vector2.zero)
+        {
+            if (inputs.sprint)
+            {
+                OnRunUpdate();
+            }
+            else
+            {
+                OnWalkUpdate();
+            }
+        }
+        else
+        {
+            OnIdleUpdate();
+        }
 
-		//  smoothing noise frequency
-		cinemachineNoise.m_FrequencyGain = Mathf.Lerp( cinemachineNoise.m_FrequencyGain, noiseFrequency, Time.deltaTime * SmoothNoiseFrequencySpeed );
-	} 
+        //  smoothing noise frequency
+        cinemachineNoise.m_FrequencyGain = Mathf.Lerp(cinemachineNoise.m_FrequencyGain, noiseFrequency, Time.deltaTime * SmoothNoiseFrequencySpeed);
+    }
 
-	public void AddItemToInventory( Item item )
-	{
-		if ( Inventory.Count >= MaxInventorySlots ) return;
+    public void AddItemToInventory(Item item)
+    {
+        if (Inventory.Count >= MaxInventorySlots) return;
 
-		//  get available position
-		int id;
-		for ( id = 0; id < MaxInventorySlots; id++ )
-		{
-			if ( !Inventory.ContainsKey( id ) )
-			{
-				break;
-			}
-		}
+        //  get available position
+        int id;
+        for (id = 0; id < MaxInventorySlots; id++)
+        {
+            if (!Inventory.ContainsKey(id))
+            {
+                break;
+            }
+        }
 
-		//  add to inventory
-		item.Owner = this;
-		item.InventoryID = id;
-		Inventory.Add( id, item );
+        //  add to inventory
+        item.Owner = this;
+        item.InventoryID = id;
+        Inventory.Add(id, item);
 
-		//  setup transform
-		item.transform.SetParent( item.transform );
-		item.gameObject.SetActive( false );
+        //  setup transform
+        item.transform.SetParent(item.transform);
+        item.gameObject.SetActive(false);
 
-		//  update inventory ui
-		if ( InventoryUI.Instance.IsVisible )
-			InventoryUI.Instance.Show();
-	}
+        //  update inventory ui
+        if (InventoryUI.Instance.IsVisible)
+            InventoryUI.Instance.Show();
+    }
 
-	public void RemoveItemFromInventory( Item item )
-	{
-		if ( item.InventoryID == -1 ) return;
+    public void RemoveItemFromInventory(Item item)
+    {
+        if (item.InventoryID == -1) return;
 
-		//  remove from inventory
-		Inventory.Remove( item.InventoryID );
-		item.Owner = null;
-		item.InventoryID = -1;
+        //  remove from inventory
+        Inventory.Remove(item.InventoryID);
+        item.Owner = null;
+        item.InventoryID = -1;
 
-		//  reset transform
-		transform.SetParent( null );
-		item.gameObject.SetActive( true );
-	}
+        //  reset transform
+        transform.SetParent(null);
+        item.gameObject.SetActive(true);
+    }
 
-	public void SwapItems( int first_id, int second_id )
-	{
-		//  wtf is this, seems like a cool way to swap
-		( Inventory[second_id], Inventory[first_id] ) = ( Inventory[first_id], Inventory[second_id] );
+    public void SwapItems(int first_id, int second_id)
+    {
+        //  wtf is this, seems like a cool way to swap
+        (Inventory[second_id], Inventory[first_id]) = (Inventory[first_id], Inventory[second_id]);
 
-		//  update IDs
-		Inventory[first_id].InventoryID = first_id;
-		Inventory[second_id].InventoryID = second_id;
-	}
+        //  update IDs
+        Inventory[first_id].InventoryID = first_id;
+        Inventory[second_id].InventoryID = second_id;
+    }
 
-	public void MoveItemTo( int source_id, int target_id )
-	{
-		//  move item
-		Inventory[target_id] = Inventory[source_id];
-		Inventory.Remove( source_id );
+    public void MoveItemTo(int source_id, int target_id)
+    {
+        //  move item
+        Inventory[target_id] = Inventory[source_id];
+        Inventory.Remove(source_id);
 
-		//  update ID
-		Inventory[target_id].InventoryID = target_id;
-	}
+        //  update ID
+        Inventory[target_id].InventoryID = target_id;
+    }
 
-	public void OnUse( InputValue input )
-	{
-		if ( !input.isPressed ) return;
-		if ( InventoryUI.Instance.IsVisible ) return;
-		if ( UseEntity == null ) return;
+    public void OnUse(InputValue input)
+    {
+        if (!input.isPressed) return;
+        if (InventoryUI.Instance.IsVisible) return;
+        if (UseEntity == null) return;
 
-		UseEntity.Use( this );
-	}
+        UseEntity.Use(this);
+    }
 
-	public void OnInventory( InputValue input )
-	{
-		if ( !input.isPressed ) return;
+    public void OnInventory(InputValue input)
+    {
+        if (!input.isPressed) return;
 
-		//  reset look
-		inputs.look = Vector2.zero;
+        //  reset look
+        inputs.look = Vector2.zero;
 
-		InventoryUI.Instance.Toggle();
-	}
+        InventoryUI.Instance.Toggle();
+    }
 
-	void OnApplicationFocus( bool hasFocus )
-	{
-		SetCursorLocked( hasFocus );
-	}
+    void OnApplicationFocus(bool hasFocus)
+    {
+        SetCursorLocked(hasFocus);
+    }
 
-	public static void SetCursorLocked( bool is_locked )
-	{
-		Cursor.lockState = is_locked ? CursorLockMode.Locked : CursorLockMode.None;
-	}
+    public static void SetCursorLocked(bool is_locked)
+    {
+        Cursor.lockState = is_locked ? CursorLockMode.Locked : CursorLockMode.None;
+    }
 }
